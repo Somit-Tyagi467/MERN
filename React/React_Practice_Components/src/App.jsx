60051034669a99026e9f41c15fe60a6832d0923a
Somit Tyagi <somittyagi559@gmail.com>
2025-09-13 05:20:39 +0530

    Initial commit

---
+++ React/React_Practice_Components/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-amber-600 text-white p-4 rounded-b-2xl text-center'>Hello</div>
+    </>
+  )
+}
+
+export default App
